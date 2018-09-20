6898fe02a6989c3134e4ef97c76e6e818e6699ec
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-09-20 10:23:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -28,7 +28,7 @@ class Application
             @@cart << new_item
             resp.write "added #{new_item}"
           else
-            resp.write "We don't have #{new_item}!"
+            resp.write "We don't have that item!"
          end
       else
         resp.write "Path Not Found"
